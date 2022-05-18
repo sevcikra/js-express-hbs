a7d74e3a525f12e10a220acda66bf698018bf614
Radim Sevcik <sevcik@adobe.com>
2022-05-18 11:05:04 +0200

    fpid cookie

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var favicon = require('serve-favicon');
 var hbs = require('hbs');
+var crypto = require('crypto');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -26,6 +27,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// set a cookie
+app.use(function (req, res, next) {
+  console.log('exjs: set a cookie:')
+  // check if client sent cookie
+  var cookie = req.cookies.FPID;
+  console.log(cookie)
+  if (cookie === undefined) {
+    // no: set a new cookie
+    randomNumber=crypto.randomUUID()
+    res.cookie('FPID',randomNumber, { maxAge: 900000, httpOnly: true });
+    console.log('cookie created successfully');
+
+  } else {
+    // yes, cookie was already present 
+    console.log('cookie exists', cookie);
+  } 
+  next(); // <-- important!
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
